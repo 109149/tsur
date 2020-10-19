6c27957859f6fd03a57ddb7f8fa8d9039d20b551
109149 <109149qwe@gmail.com>
2020-10-19 23:05:52 +0400

    version update

--- src/consts.rs
+++ src/consts.rs
@@ -1,3 +1,3 @@
-pub const VERSION: &str = "0.1.1";
+pub const VERSION: &str = "0.1.2";
 pub const AUTHOR: &str = "109149 <109149qwe@gmail.com>";
 pub const ABOUT: &str = "Just Rusting...You know...O_O";
